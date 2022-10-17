628c9ab4cd31ad33bbd71234f8cdf308261577ac
valentin Glass <valentinglass@mail.ru>
2022-10-17 21:09:45 +0400

    add cod3

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
         double rubles = 48562;
         double rateUSD = 74.2;
         double dollars = rubles / rateUSD;
-        //System.out.printf("В вашем кошельке " + rubles + " рублей" + "В долларах это - " + dollars + " долларов");
-        /*System.out.printf("В вашем кошельке %.2f",rubles);
+        /*
+        System.out.printf("В вашем кошельке " + rubles + " рублей" + "В долларах это - " + dollars + " долларов");
+        System.out.printf("В вашем кошельке %.2f",rubles);
         System.out.printf(" рублей В долларах это -  %.2f",dollars);
-        System.out.printf(" долларов");*/
+        System.out.printf(" долларов");
+        */
         System.out.printf("\nВ вашем кошельке " + rubles + " рублей." + " В долларах это - %.2f",dollars);
         System.out.print(" долларов");
 
